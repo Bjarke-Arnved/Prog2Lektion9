187169631dd5518d763ddffc78bfacdbf324c0e2
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-02-26 09:27:46 +0100

    Completed exercise 1

---
+++ src/opgave01/models/StringVault.java
@@ -0,0 +1,19 @@
+package opgave01.models;
+public class StringVault {
+    private String secret;
+    private String password;
+
+    public StringVault(String secret, String password) {
+        this.secret = secret;
+        this.password = password;
+    }
+
+    public String getSecret(String password) {
+        if(password.equals(this.password)) {
+            return secret;
+        }
+        else {
+            throw new IllegalArgumentException("Wrong password");
+        }
+    }
+}
